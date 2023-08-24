dcf477c59be5c9c5dfcdf9650c10bb9918c3c4a5
Mindul <pencake33@naver.com>
2023-08-24 17:37:11 +0900

    타입 문제 해결

--- model/model.py
+++ model/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, TEXT, BIGINT
+from sqlalchemy import Column, TEXT, BIGINT, BOOLEAN
 from sqlalchemy.orm import  declarative_base
   
 Base = declarative_base()
@@ -33,4 +33,4 @@ class Person(Base):
   name = Column(TEXT, nullable=False)
   phoneNumber = Column(TEXT, nullable=False)
   type = Column(TEXT, nullable=False)
-  agree = Column(bool, nullable=False)
+  agree = Column(BOOLEAN, nullable=False)
